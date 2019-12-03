6a9df6eda089556cdb96ed640c60ed17aca6bb06
Tian Lei <553948327@qq.com>
2019-12-03 11:16:07 +0800

    添加Prometheus配置

--- NodeExporter/startup.sh
+++ NodeExporter/startup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-for i in {1..256}
-do
-	#EXPORTER_PORT=$((9100+$i))
-	docker run \
-		--name node$i \
-		--hostname node$i \
-		--network apache_network \
-		--restart always \
-		-v "/proc:/host/proc:ro" \
-		-v "/sys:/host/sys:ro" \
-		-v "/:/rootfs:ro" \
-		-d prom/node-exporter
-done
+case "$1" in 
+	"start")
+		for i in {1..192}
+		do
+			#EXPORTER_PORT=$((9100+$i))
+			docker run \
+				--name node_exporter$i \
+				--hostname node$i \
+				--network apache_network \
+				--restart always \
+				-v "/proc:/host/proc:ro" \
+				-v "/sys:/host/sys:ro" \
+				-v "/:/rootfs:ro" \
+				-d prom/node-exporter
+		done
+		;;
+	"stop")
+		docker ps|grep node_exporter|awk '{print $NF}'|xargs -I {} docker rm -f {}
+		;;
+	*)
+		echo "Usage: $@ [start|stop]"
+		;;		
+esac
--- Prometheus/startup.sh
+++ Prometheus/startup.sh
@@ -2,15 +2,32 @@
 
 readonly WORKDIR=`pwd`
 
+
+
 if [ ! -d $WORKDIR/prometheus ];then
 	mkdir -p $WORKDIR/prometheus
 fi
 
-docker run  \
-	--name prometheus01 \
-	--network apache_network \
-	--hostname prometheus01 \
-	--restart always \
-	-p 9090:9090 \
-	-v $WORKDIR/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml  \
-	-d prom/prometheus
+case "$1" in
+	"start")
+		docker ps|grep node_exporter|awk '{print $NF}'|xargs -I {} docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' {} >$WORKDIR/prometheus/file_sd/server.json
+		sed -i -e 's@^192@\t\t\t"192@g' -e 's@$@:9100",@g' -e '$s/,//' $WORKDIR/prometheus/file_sd/server.json
+		sed -i -e "1c[\n\t{\n\t\"targets\":[" $WORKDIR/prometheus/file_sd/server.json
+		sed -i -e '$a]\n\t\t}\n\t]' $WORKDIR/prometheus/file_sd/server.json
+		docker run  \
+			--name prometheus01 \
+			--network apache_network \
+			--hostname prometheus01 \
+			--restart always \
+			-p 9090:9090 \
+			-v $WORKDIR/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml  \
+			-v $WORKDIR/prometheus/file_sd:/prometheus/file_sd/ \
+			-d prom/prometheus
+		;;
+	"stop")
+		docker rm -f prometheus01
+		;;
+	*)
+		echo "Usage: $@ [start|stop]"
+		;;
+esac
