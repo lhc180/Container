3e8e8e5e53e8d7c9d85d7747be382c036fac5ada
Tian Lei <553948327@qq.com>
2019-12-14 18:13:18 +0800

    把adapter的端口由环境变量传入

--- KafkaAdapter/startup.sh
+++ KafkaAdapter/startup.sh
@@ -5,11 +5,11 @@ case "$1" in
 	"start")
 		docker run \
 			--name kafka_adapter \
-			--network apache_network \
+			--network host \
 			--restart always \
 			--hostname kafka_adapter \
 			--cpus 2 \
-			-p 9080:8080 \
+			-e PORT=9080 \
 			-e KAFKA_BROKER_LIST="172.18.8.250:9093,172.18.8.250:9094,172.18.8.250:9095" \
 			-e KAFKA_TOPIC="prom_metrics" \
 			-d telefonica/prometheus-kafka-adapter:1.4.0
